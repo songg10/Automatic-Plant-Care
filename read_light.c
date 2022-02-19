16d7ec6a0432e72d35a8fc942db649d08866384d
springbro294 <66364910+springbro294@users.noreply.github.com>
2022-02-18 20:43:26 -0800

    Update read_light.c
    re-upload read_light due to error in editing

--- read_light.c
+++ read_light.c
@@ -1,44 +1,61 @@
-from glob import glob
-from smtplib import SMTP
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+#define A2D_FILE_VOLTAGE "/sys/bus/iio/devices/iio:device0/in_voltage1_raw"
+#define A2D_VOLTAGE_REF_V 3.3
+#define A2D_MAX_READING 4095
+
+double sqr( double x ) { return x * x; }
+// meaning of 'precision': the returned answer should be base^x, where
+//                         x is in [power-precision/2,power+precision/2]
+double mypow( double base, double power, double precision )
+{   
+   if ( power < 0 ) return 1 / mypow( base, -power, precision );
+   if ( power >= 10 ) return sqr( mypow( base, power/2, precision/2 ) );
+   if ( power >= 1 ) return base * mypow( base, power-1, precision );
+   if ( precision >= 1 ) return sqrt( base );
+   return sqrt( mypow( base, power*2, precision*2 ) );
+}
+
+int getVoltageReading()
+{
+    // Open file
+    FILE *f = fopen(A2D_FILE_VOLTAGE, "r");
+    if (!f)
+    {
+        printf("ERROR: Unable to open voltage input file. Cape loaded?\n");
+        printf(" Check /boot/uEnv.txt for correct options.\n");
+        exit(-1);
+    }
+    // Get reading
+    int a2dReading = 0;
+    int itemsRead = fscanf(f, "%d", &a2dReading);
+    if (itemsRead <= 0)
+    {
+        printf("ERROR: Unable to read values from voltage input file.\n");
+        exit(-1);
+    }
+    // Close file
+    fclose(f);
 
 
-MAX_LENGTH=2048
+    return a2dReading;
+}
 
-recipient_address=None
-sender_address='plant.care.alert@gmail.com'
-sender_pw='plantcare433!'
+double voltageToLumen (int voltage) {
+    double lux = ((double)voltage / A2D_MAX_READING) * A2D_VOLTAGE_REF_V;
+    return 27.565 * mypow(10, lux, .000001);
+}
 
-def set_email_address(email):
-    global recipient_address
-    recipient_address=email  
 
-def init_email():
-    while 1:
-        email=input('Input your email address: ')
-        print('Email entered is: ', email)
-        confirm=input('Is this the correct email? (y/n): ')
-        while (confirm != 'y' and confirm != 'Y' and confirm != 'n' and confirm != 'N'):
-            confirm=input("Enter \"y\" for yes, \"n\" for no: ")
-        if confirm == 'y' or confirm == 'Y':
-            set_email_address(email)
-            return
-
-def set_message(subject, text):
-    global message
-    message = 'Subject: {}\n\n{}'.format(subject, text)
-
-def send_email():
-    server=SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender_address, sender_pw)
-    SMTP.sendmail(self=server, from_addr=sender_address, to_addrs=recipient_address, msg=message)
-    print('sent email.')
-    server.quit()
-
-def main():
-    init_email()
-    set_message('testing', 'testing 123')
-    send_email()
-    
-if __name__ == '__main__':
-    main()
+int main () {
+    while (1) {
+        printf("lumens currently is:  %f\n", voltageToLumen(getVoltageReading()));
+        printf("voltag currently is:  %f\n", ((double)getVoltageReading() / A2D_MAX_READING) * 5);
+        sleep(1);
+    }
+    return 0;
+}
