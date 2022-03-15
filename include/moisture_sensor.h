046fa583db40b90425c20a92ab74afac6806360d
stn5 <stn5@sfu.ca>
2022-03-15 02:56:50 -0700

    Added implementation for reading the moisture sensor value through I2C

---
+++ include/moisture_sensor.h
@@ -0,0 +1,12 @@
+// Header defining functions to read the value of the moisture sensor.
+
+#ifndef _MOIST_SENSOR_H_
+#define _MOIST_SENSOR_H_
+
+// Return the moisture level
+int Moisture_getMoistLevel(void);
+
+// Return the temperature in Celsius
+unsigned int Moisture_getTemp(void);
+
+#endif 
