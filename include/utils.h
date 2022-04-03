b316d418c5ece3aeb08128659c0fa76c64af096d
Sara Hwang <83997604+sha173@users.noreply.github.com>
2022-04-02 18:48:38 -0700

    front end support for age utilities

--- include/utils.h
+++ include/utils.h
@@ -5,12 +5,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "backup.h"
 
 // Utility functions to read from or write to file
 int Utils_readFromFileToScreen(const char *fileName);
 void Utils_writeToFile(const char *fileName, const char *command);
-int Utils_read_bin(const char* fileName, char* buffer);
-int Utils_write_bin(const char* fileName, const char* value, int size);
 
 #endif
